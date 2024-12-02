d1b686a99165206af51c2a15c8fd6f722404343d
Thomas Rory Gummerson <thomas.gummerson@piql.com>
2024-12-02 12:50:11 +0100

    hotfix: patch util.isDate for node 23

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // @ts-check
 'use strict';
 
+// NeDB on life support, util.isDate is removed in node 23.x, monkeypatch it
+// eslint-disable-next-line global-require
+if (!('isDate' in require('util'))) {
+	// eslint-disable-next-line global-require
+	require('util').isDate = require('util').types.isDate;
+}
+
 process.chdir(__dirname);
 require('ts-node').register({ transpileOnly: true });
 
